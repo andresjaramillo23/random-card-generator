dd8ffb27c08f2bf17c81709ab9ebd7a2cf834ed5
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-10-08 04:25:12 +0000

    Added button

--- src/app.js
+++ src/app.js
@@ -8,4 +8,35 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.getElementById("cardIcon").innerHTML = "♣";
+  document.getElementById("cardNumber").innerHTML = "1";
+};
+
+function getRandomCard() {
+  return Math.floor(Math.random() * 4 + 1);
+}
+
+function getRandomNumber() {
+  return String(Math.floor(Math.random() * 13));
+}
+
+const btn = document.getElementById("b1");
+
+btn.onclick = function() {
+  switch (getRandomCard()) {
+    case 1:
+      document.getElementById("cardIcon").innerHTML = "♦";
+      break;
+    case 2:
+      document.getElementById("cardIcon").innerHTML = "♥";
+      break;
+    case 3:
+      document.getElementById("cardIcon").innerHTML = "♠";
+      break;
+    case 4:
+      document.getElementById("cardIcon").innerHTML = "♣";
+      break;
+  }
+
+  document.getElementById("cardNumber").innerHTML = getRandomNumber();
 };
